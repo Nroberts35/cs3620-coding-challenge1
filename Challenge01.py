48c098a22b19e72bfdf0781ae0c2dcecb2f8c7d3
Nikolas Roberts <nikolasroberts35@gmail.com>
2026-01-06 12:56:25 -0700

    Testing push

---
+++ Challenge01.py
@@ -0,0 +1,9 @@
+#Part 1
+principle = int(input("Enter Principle : "))
+numOfYears = int(input("Enter Number of Years : "))
+interestRate = int(input("Enter Interest Rate : "))
+
+simpleInterestRate = (principle * numOfYears *interestRate) / 100
+print(simpleInterestRate)
+
+# Part 2
